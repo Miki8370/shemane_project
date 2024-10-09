16fba4e423ab771b0ff57071d0d7366f1c890288
MikiWon <mikiyaswondim@icloud.com>
2024-10-08 23:40:36 -0700

    Now it renders custom design image in the home page

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views import View
+from .models import Design
 
 # Create your views here.
 
-class HomePage(TemplateView):
-    template_name = 'home.html'
+class HomePage(View):
+    def get(self, request):
+        designs = Design.objects.all()
+        return render(request, 'home.html', {'designs': designs})
+        
     
